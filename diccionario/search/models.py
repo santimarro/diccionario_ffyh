598cbb2b2212d927b59bf0572dddecb0cdd2af59
Santiago Marro <smarro@gmail.com>
2018-08-18 15:12:23 -0300

    Added name display method to models

--- diccionario/search/models.py
+++ diccionario/search/models.py
@@ -10,12 +10,21 @@ class Word(models.Model):
     word_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.word_text
+
 
 class Meaning(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
     meaning_text = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.meaning_text
+
 
 class Example(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
     example_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.example_text
